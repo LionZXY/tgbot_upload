631beda136ec67905a29ffad939836d9d3bd4ca6
Nikita Kulikov <n@kulikov.uk>
2025-05-05 19:57:58 +0100

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { TelegramClient } from '@mtcute/node'
+
+import * as env from './env.js'
+
+const tg = new TelegramClient({
+    apiId: env.API_ID,
+    apiHash: env.API_HASH,
+    storage: 'bot-data/session',
+})
+
+
+const user = await tg.start({ botToken: env.BOT_TOKEN })
+console.log('Logged in as', user.username)
